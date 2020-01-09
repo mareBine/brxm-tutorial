0411ccc96fa1dca9805601f90c6fcbe4432fc269
Marko Kovačič <marko.kovacic@comtrade.com>
2020-01-09 10:37:24 +0100

    initial commit

---
+++ site/components/src/main/java/org/example/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.example.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="test1:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
